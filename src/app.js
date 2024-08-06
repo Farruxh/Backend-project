2d9dfb17a60cda35b411fa6cddb1c67ddc0e36aa
Farrukh <farrukh.web2@gmail.com>
2024-08-06 17:29:31 +0500

    Setup configuration

--- src/app.js
+++ src/app.js
@@ -12,4 +12,13 @@ app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(express.static("public")) //for accepting and storing public asset(images etc)
 app.use(cookieParser())
+
+// routes import
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users",userRouter)
+
+//this will like: http://localhost:8000/api/v1/users
+
 export {app}
